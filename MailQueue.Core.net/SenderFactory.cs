3219cc94b06539507ba2679396a373ab182b4c2c
Daniel Cohen Gindi <danielgindi@gmail.com>
2018-11-20 11:15:00 +0200

    Added an ISender factory

---
+++ MailQueue.Core.net/SenderFactory.cs
@@ -0,0 +1,39 @@
+﻿using MailQueue.Senders;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MailQueue
+{
+    public static class SenderFactory
+    {
+        public static ISender GetSenderForSettings(IMailServerSettings settings)
+        {
+            if (settings is SmtpMailServerSettings)
+            {
+                return new Senders.SMTP();
+            }
+            else if (settings is MailgunMailServerSettings)
+            {
+                return new Senders.Mailgun();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Sends an email with a MailMessage and a set of settings.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="settings"></param>
+        /// <returns>true if succeeded, false if sending was skipped (i.e missing settings). Could throw exceptions for other reasons.</returns>
+        public static async Task<bool> SendMail(MailMessage message, IMailServerSettings settings)
+        {
+            var sender = GetSenderForSettings(settings);
+            if (sender == null) return false;
+
+            return await sender.SendMailAsync(message, settings);
+        }
+    }
+}
--- MailQueue.Core.net/Senders/ISender.cs
+++ MailQueue.Core.net/Senders/ISender.cs
@@ -5,6 +5,12 @@ namespace MailQueue.Senders
 {
     public interface ISender
     {
+        /// <summary>
+        /// Sends an email with a MailMessage and a set of settings.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="settings"></param>
+        /// <returns>true if succeeded, false if sending was skipped (i.e missing settings). Could throw exceptions for other reasons.</returns>
         Task<bool> SendMailAsync(MailMessage message, IMailServerSettings settings);
     }
 }
